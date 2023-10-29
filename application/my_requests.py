c5ad4f022d19bee0e2391f1e58f2a637caa5928a
Jess <vboxuser@ubuntu-22.04.3.myguest.virtualbox.org>
2023-10-29 15:14:41 +0000

    modified my_requests.py

--- application/my_requests.py
+++ application/my_requests.py
@@ -19,13 +19,13 @@ class MyRequests:
         return self.request_data()
 
     # method that calls data from the "name" endpoint?
-    def name_endpoint(self):
-        self.url = f"{self.base_url}name"
+    def name_endpoint(self, name):
+        self.url = f"{self.base_url}name/{name}"
         return self.request_data()
 
     # method that calls data from the "VariantValidator" endpoint?
     def VariantValidator_endpoint(self, genome_build, variant_description, select_transcripts):
-        self.url = '/'.join([self.base_url, genome_build, variant_description, select_transcripts])
+        self.url = f"{self.base_url}VariantValidator/variantvalidator/{genome_build}/{variant_description}/{select_transcripts}"
         return self.request_data()
 
 
@@ -35,11 +35,26 @@ if __name__ == "__main__":
     # Set the base url
     mrq.base_url = "http://127.0.0.1:5000/"
 
-    # request the data
-    response = mrq.hello()
+    # request the data from different endpoints
+    response_1 = mrq.hello()
+    response_2 = mrq.name_endpoint("Bob")
+    response_3 = mrq.VariantValidator_endpoint("GRCh37", "NM_000088.3:c.589G>T", "'all'")
 
-    # print the 3 response sections
-    print(response.status_code)
-    print(response.headers)
-    print(response.text)
-    print(response.json())
+    # method that prints the 3 response sections
+    def print_response(response):
+        print(response.status_code)
+        print(response.headers)
+        print(response.text)
+        print(response.json())
+
+
+    # call data using responses
+    print_response(response_1)
+    print_response(response_2)
+    print_response(response_3)
+
+
+# How to run:
+# conda activate variantvalidator
+# python app_v5.py
+# python my_requests.py
